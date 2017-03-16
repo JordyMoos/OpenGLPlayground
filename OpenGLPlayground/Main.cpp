d664c3415f3e170a7b4e555ae9f1e38be4bc2ce9
JordyMoos <jordymoos@gmail.com>
2017-03-16 22:21:03 +0100

    Creating the window

--- OpenGLPlayground/Main.cpp
+++ OpenGLPlayground/Main.cpp
@@ -5,6 +5,18 @@
 #include <GLFW/glfw3.h>
 
 
+GLFWwindow* window = nullptr;
+
+
+void keyCallback(GLFWwindow*, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+
 int main(int argc, char* args[])
 {
 	glfwInit();
@@ -13,7 +25,41 @@ int main(int argc, char* args[])
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+	window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		printf("Failed to create GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetKeyCallback(window, keyCallback);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Failed to initialize GLEW");
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(255, 0, 0, 255);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
 	return 0;
 }
+
+
 
 
