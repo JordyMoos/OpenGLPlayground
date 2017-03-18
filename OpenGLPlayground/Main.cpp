7aac1e19b48dcc1e55052bee954d504fc213ecee
JordyMoos <jordymoos@gmail.com>
2017-03-18 19:08:23 +0100

    Adding the triangle and also color it

--- OpenGLPlayground/Main.cpp
+++ OpenGLPlayground/Main.cpp
@@ -15,16 +15,19 @@ void keyCallback(GLFWwindow*, int key, int scancode, int action, int mode);
 
 const GLchar* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 position;\n"
+	"out vec4 color;\n"
 	"void main()\n"
 	"{\n"
+	"color = vec4(clamp(position, 0.0, 1.0), 1.0);\n"
 	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
 	"}\n\0";
 
 const GLchar* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 color;\n"
+	"in vec4 color;\n"
+	"out vec4 fragColor;\n"
 	"void main()\n"
 	"{\n"
-	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"fragColor = color;\n"
 	"}\n\0";
 
 
@@ -96,6 +99,15 @@ int main(int argc, char* args[])
 		return -1;
 	}
 
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
 	glUseProgram(shaderProgram);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
@@ -107,8 +119,13 @@ int main(int argc, char* args[])
 	{
 		glfwPollEvents();
 
-		glClearColor(255, 0, 0, 255);
+		glClearColor(0, 0, 0, 255);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
